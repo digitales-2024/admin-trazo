eee88fa142acc6a8c6f306102073094e981e295f
Diego Rondón <furrtr4532@gmail.com>
2024-10-23 15:45:36 -0500

    refactor: configurar el componente Menu para eliminar estilos innecesarios

--- src/components/admin-panel/Menu.tsx
+++ src/components/admin-panel/Menu.tsx
@@ -22,14 +22,14 @@ interface MenuProps {
 }
 
 export const Menu = ({ isOpen }: MenuProps) => {
-    const pathname = usePathname() ?? ""; // Si pathname es null, se usará ""
+    const pathname = usePathname() ?? "";
     const menuList = getMenuList(pathname);
 
     const { signOut } = useLogout();
 
     return (
         <ScrollArea className="[&>div>div[style]]:!block">
-            <nav className="mt-8 h-full w-full">
+            <nav className="h-full w-full">
                 <ul className="flex min-h-[calc(100vh-48px-36px-16px-32px)] flex-col items-start space-y-1 px-2 lg:min-h-[calc(100vh-32px-40px-32px)]">
                     {menuList.map(({ groupLabel, menus }, index) => (
                         <li
