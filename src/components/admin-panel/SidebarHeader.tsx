6df15061cdd1ba2113b1c9fb9881f191565c7ea5
Diego Rondón <furrtr4532@gmail.com>
2024-10-23 16:54:08 -0500

    feat: agregar componentes LogoTrazo y LogoTrazoTriangule al encabezado de la barra lateral

--- src/components/admin-panel/SidebarHeader.tsx
+++ src/components/admin-panel/SidebarHeader.tsx
@@ -1,4 +1,5 @@
-import Image from "next/image";
+import { LogoTrazo } from "@/assets/icons/LogoTrazo";
+import { LogoTrazoTriangule } from "@/assets/icons/LogoTrazoTriangule";
 import Link from "next/link";
 
 interface SidebarHeaderProps {
@@ -9,16 +10,14 @@ export const SidebarHeader = ({ isOpen }: SidebarHeaderProps) => {
     return (
         <Link href="/">
             <div
-                className={`cursor-pointer px-8 py-4 ${isOpen ? "open" : "closed"}`}
+                className={` ${isOpen ? "open px-8 py-3" : "closed items-center justify-center py-2"} flex items-center justify-center`}
             >
-                <div className="rounded bg-black p-2">
-                    <Image
-                        className="inline-block"
-                        src="https://www.trazoarq.com/wp-content/uploads/2021/05/logo-30.svg"
-                        alt="Logo Trazo"
-                        width={100}
-                        height={100}
-                    />
+                <div className="inline-flex items-center justify-center justify-items-center rounded bg-black p-2">
+                    {isOpen ? (
+                        <LogoTrazo className="h-10" />
+                    ) : (
+                        <LogoTrazoTriangule className="hover:animate-bounce" />
+                    )}
                 </div>
             </div>
         </Link>
