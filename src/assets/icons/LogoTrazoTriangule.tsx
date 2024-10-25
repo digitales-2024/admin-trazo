7404106c708a66a257e8e49e2e696a8245b288d9
Diego Rondón <furrtr4532@gmail.com>
2024-10-25 09:34:06 -0500

    feat: actualizar tamaño del logo en el componente LogoTrazoTriangule

--- src/assets/icons/LogoTrazoTriangule.tsx
+++ src/assets/icons/LogoTrazoTriangule.tsx
@@ -3,12 +3,12 @@ import React, { SVGProps } from "react";
 export const LogoTrazoTriangule = (props: SVGProps<SVGSVGElement>) => (
     <svg
         xmlns="http://www.w3.org/2000/svg"
-        width={30}
-        height={25}
+        width={22}
+        height={19}
         fill="none"
         {...props}
     >
-        <path fill="#fff" d="M17.143.032h-4.286l12.857 24.926H30L17.143.032Z" />
-        <path fill="#BE2126" d="m12.93 0 12.94 25H0L12.93 0Z" />
+        <path fill="#fff" d="M12.571.024H9.43l9.428 18.944H22L12.571.024Z" />
+        <path fill="#BE2126" d="m9.482 0 9.488 19H0L9.482 0Z" />
     </svg>
 );
