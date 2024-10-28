828b3556aa5e3da9f428c60e5a74c67c4e3209d5
Diego Rondón <furrtr4532@gmail.com>
2024-10-28 12:37:34 -0500

    feat: agregar tipo de cliente con descripción

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,5 +7,5 @@ export type {
     Permission,
     ModulePermissions,
 } from "./roles";
-export type { Client } from "./client";
+export type { Client, ClientWithDescription } from "./client";
 export type { City } from "./city";
