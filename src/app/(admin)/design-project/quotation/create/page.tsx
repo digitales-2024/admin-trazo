749f674cd73272b860f69e6a89b3f1a25df21f51
Diego Rondón <furrtr4532@gmail.com>
2024-10-30 10:00:07 -0500

    feat: agregar manejo de pisos y cálculo de metros totales en la página de creación de cotizaciones

--- src/app/(admin)/design-project/quotation/create/page.tsx
+++ src/app/(admin)/design-project/quotation/create/page.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+import { Floor } from "@/types";
+import React, { useState } from "react";
 
 import { HeaderPage } from "@/components/common/HeaderPage";
 import { Shell } from "@/components/common/Shell";
@@ -6,14 +8,31 @@ import IntegralProject from "@/components/quotation/create-quotation/create-inte
 import { CreateLevelSpace } from "@/components/quotation/create-quotation/create-level-space/LevelSpaceCreate";
 
 export default function CreateQuotationPage() {
+    const [floors, setFloors] = useState<Floor[]>([
+        { number: 1, name: "Nivel 1", spaces: [], expanded: true },
+    ]);
+
+    const calculateTotalBuildingMeters = () => {
+        return floors.reduce(
+            (total, floor) =>
+                total +
+                floor.spaces.reduce((sum, space) => sum + space.meters, 0),
+            0,
+        );
+    };
+
     return (
         <Shell className="gap-6">
             <HeaderPage
                 title="Crear Cotización"
                 description="Complete todos los campos para crear una cotización."
             />
-            <CreateLevelSpace />
-            <IntegralProject />
+            <CreateLevelSpace
+                floors={floors}
+                setFloors={setFloors}
+                calculateTotalBuildingMeters={calculateTotalBuildingMeters}
+            />
+            <IntegralProject area={calculateTotalBuildingMeters()} />
         </Shell>
     );
 }
