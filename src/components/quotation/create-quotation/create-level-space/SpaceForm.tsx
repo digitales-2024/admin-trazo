fd366cd3fe2631c0dffc24e6dd939079b1fbe42b
Diego Rondón <furrtr4532@gmail.com>
2024-10-31 09:53:31 -0500

    feat: agregar funcionalidad de selección en SpaceForm y mejorar la interfaz

--- src/components/quotation/create-quotation/create-level-space/SpaceForm.tsx
+++ src/components/quotation/create-quotation/create-level-space/SpaceForm.tsx
@@ -2,20 +2,12 @@
 
 import { useSpaces } from "@/hooks/use-space";
 import { Space } from "@/types";
-import { Trash } from "lucide-react";
 import * as React from "react";
 
-import { Button } from "@/components/ui/button";
+import { AutoComplete } from "@/components/ui/autocomplete";
+import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import {
-    Tooltip,
-    TooltipContent,
-    TooltipProvider,
-    TooltipTrigger,
-} from "@/components/ui/tooltip";
-
-import { Combobox } from "./ComboboxSpace";
 
 interface EnvironmentFormProps {
     space: Space;
@@ -24,10 +16,9 @@ interface EnvironmentFormProps {
     updateEnvironment: (
         floorNumber: number,
         environmentIndex: number,
-        field: "name" | "meters" | "amount",
-        value: string | number,
+        field: "name" | "meters" | "amount" | "selected",
+        value: string | number | boolean,
     ) => void;
-    deleteSpace: (floorNumber: number, environmentIndex: number) => void;
 }
 
 export function SpaceForm({
@@ -35,12 +26,18 @@ export function SpaceForm({
     floorNumber,
     environmentIndex,
     updateEnvironment,
-    deleteSpace,
 }: EnvironmentFormProps) {
     const { dataSpacesAll = [] } = useSpaces();
     const [selectedSpace, setSelectedSpace] = React.useState(space.name);
     const [amount, setAmount] = React.useState(space.amount || 1);
     const [meters, setMeters] = React.useState(space.meters);
+    const [isSelected, setIsSelected] = React.useState<boolean>(
+        space.selected || false,
+    );
+
+    React.useEffect(() => {
+        setIsSelected(space.selected || false);
+    }, [space.selected]);
 
     const handleAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const value = Math.max(1, Number(e.target.value));
@@ -54,71 +51,85 @@ export function SpaceForm({
         updateEnvironment(floorNumber, environmentIndex, "meters", value);
     };
 
+    // Ajuste del manejador para onCheckedChange
+    const handleCheckboxChange = (checked: boolean) => {
+        setIsSelected(checked);
+        updateEnvironment(floorNumber, environmentIndex, "selected", checked);
+    };
+
     return (
-        <div className="flex items-center gap-4 lg:grid-cols-6">
-            <div className="w-full">
-                <Label htmlFor={`amount-${floorNumber}-${environmentIndex}`}>
-                    Cantidad
-                </Label>
-                <Input
-                    id={`amount-${floorNumber}-${environmentIndex}`}
-                    type="number"
-                    value={amount}
-                    onChange={handleAmountChange}
-                    placeholder="Cantidad"
+        <div className="grid grid-cols-1 items-center gap-2 pb-4">
+            <div className="flex flex-row items-end justify-end gap-2">
+                <Checkbox
+                    id={`checkbox-${floorNumber}-${environmentIndex}`}
+                    checked={isSelected}
+                    onCheckedChange={handleCheckboxChange}
+                    className="mb-4"
                 />
+                <div>
+                    <Label
+                        className="truncate"
+                        htmlFor={`amount-${floorNumber}-${environmentIndex}`}
+                    >
+                        Cantidad
+                    </Label>
+                    <Input
+                        id={`amount-${floorNumber}-${environmentIndex}`}
+                        type="number"
+                        value={amount}
+                        onChange={handleAmountChange}
+                        placeholder="Cantidad"
+                        className="w-16"
+                    />
+                </div>
+                <div className="w-full">
+                    <Label
+                        className="truncate"
+                        htmlFor={`environment-${floorNumber}-${environmentIndex}`}
+                    >
+                        Nombre
+                    </Label>
+
+                    <AutoComplete
+                        options={dataSpacesAll.map((space) => ({
+                            value: space.name,
+                            label: space.name,
+                        }))}
+                        placeholder="Selecciona un espacio"
+                        emptyMessage="No se encontraron espacios"
+                        value={{
+                            value: selectedSpace,
+                            label: selectedSpace,
+                        }}
+                        onValueChange={(option) => {
+                            setSelectedSpace(option.value);
+                            updateEnvironment(
+                                floorNumber,
+                                environmentIndex,
+                                "name",
+                                option.value,
+                            );
+                        }}
+                        className="z-50"
+                    />
+                </div>
+                <div className="">
+                    <Label
+                        className="truncate"
+                        htmlFor={`meters-${floorNumber}-${environmentIndex}`}
+                    >
+                        Área (m²)
+                    </Label>
+                    <Input
+                        id={`meters-${floorNumber}-${environmentIndex}`}
+                        type="number"
+                        value={meters}
+                        onChange={handleMetersChange}
+                        placeholder="m²"
+                        className="w-14"
+                    />
+                </div>
             </div>
-            <div className="col-span-3">
-                <Label
-                    htmlFor={`environment-${floorNumber}-${environmentIndex}`}
-                >
-                    Nombre
-                </Label>
-                <Combobox
-                    dataSpacesAll={dataSpacesAll}
-                    value={selectedSpace}
-                    setValue={(value) => {
-                        setSelectedSpace(value);
-                        updateEnvironment(
-                            floorNumber,
-                            environmentIndex,
-                            "name",
-                            value,
-                        );
-                    }}
-                />
-            </div>
-            <div className="col-span-2">
-                <Label htmlFor={`meters-${floorNumber}-${environmentIndex}`}>
-                    Metros cuadrados
-                </Label>
-                <Input
-                    id={`meters-${floorNumber}-${environmentIndex}`}
-                    type="number"
-                    value={meters}
-                    onChange={handleMetersChange}
-                    placeholder="m²"
-                />
-            </div>
-            <TooltipProvider>
-                <Tooltip>
-                    <TooltipTrigger asChild>
-                        <Button
-                            variant="ghost"
-                            size="sm"
-                            className="ml-2"
-                            onClick={() =>
-                                deleteSpace(floorNumber, environmentIndex)
-                            }
-                        >
-                            <Trash className="h-4 w-4" />
-                        </Button>
-                    </TooltipTrigger>
-                    <TooltipContent>
-                        <span>Eliminar ambiente</span>
-                    </TooltipContent>
-                </Tooltip>
-            </TooltipProvider>
         </div>
     );
 }
