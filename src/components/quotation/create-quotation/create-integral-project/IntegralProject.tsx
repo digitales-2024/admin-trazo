382321d057c35929fee3f074a5914afd6b69ac36
Diego Rondón <furrtr4532@gmail.com>
2024-11-08 16:14:56 -0500

    feat: eliminar definición de proyectos y mover datos a un archivo separado

--- src/components/quotation/create-quotation/create-integral-project/IntegralProject.tsx
+++ src/components/quotation/create-quotation/create-integral-project/IntegralProject.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Costs, IntegralProjectItem, QuotationStructure } from "@/types";
+import { Costs, QuotationStructure } from "@/types";
 import { ChevronDown, ChevronUp, PencilRuler } from "lucide-react";
 import React, { useState } from "react";
 import { UseFormReturn } from "react-hook-form";
@@ -18,6 +18,7 @@ import {
     CollapsibleTrigger,
 } from "@/components/ui/collapsible";
 
+import { projectNames, projects } from "../../IntegralProjectData";
 import CostSummary from "./CostSummary";
 import IntegralProjectTable from "./IntegralProjectTable";
 
@@ -33,13 +34,6 @@ interface IntegralProjectProps
     form: UseFormReturn<QuotationStructure>;
 }
 
-const projectNames: { [key in keyof Costs]: string } = {
-    architecturalCost: "Proyecto Arquitectónico",
-    structuralCost: "Proyecto Estructural",
-    electricCost: "Proyecto de Instalaciones Eléctricas",
-    sanitaryCost: "Proyecto de Instalaciones Sanitarias",
-};
-
 export default function IntegralProject({
     area,
     costs,
@@ -53,48 +47,20 @@ export default function IntegralProject({
     const [isOpen, setIsOpen] = useState<boolean>(true);
 
     const subtotal = Object.values(costs).reduce((sum, cost) => sum + cost, 0);
+
+    console.log("area de integral", area);
+    console.log("costos", subtotal);
+    console.log("descuento", discount);
+    console.log("tipo de cambio", exchangeRate);
     const totalCost = discount * exchangeRate * area;
+
+    console.log("costo total", totalCost);
 
     const handleCostChange = (project: keyof Costs, value: number) => {
         setCosts((prev) => ({
             ...prev,
             [project]: value || 0,
         }));
-    };
-
-    const projects: { [key: string]: IntegralProjectItem[] } = {
-        "Proyecto Arquitectónico": [
-            {
-                description: "Plano de Ubicación y Localización",
-                unit: "escala 1/1000",
-            },
-            { description: "Plano de Diferentes Niveles", unit: "escala 1/50" },
-            { description: "Plano de Elevaciones", unit: "escala 1/50" },
-            { description: "Plano de Cortes", unit: "escala 1/50" },
-            { description: "Memoria Descriptiva", unit: "" },
-        ],
-        "Proyecto Estructural": [
-            { description: "Plano de Cimentación", unit: "escala 1/50" },
-            {
-                description: "Plano de Detalles Constructivos",
-                unit: "escala 1/50",
-            },
-            {
-                description: "Plano de Aligerado de Niveles",
-                unit: "escala 1/50",
-            },
-            { description: "Memoria Descriptiva", unit: "" },
-        ],
-        "Proyecto de Instalaciones Eléctricas": [
-            { description: "Planos de Tendido Eléctrico", unit: "escala 1/50" },
-            { description: "Cálculo Eléctrico", unit: "" },
-            { description: "Memoria Justificativa", unit: "" },
-        ],
-        "Proyecto de Instalaciones Sanitarias": [
-            { description: "Planos de Tendido Sanitario", unit: "escala 1/50" },
-            { description: "Planos de Agua y Desagüe", unit: "escala 1/50" },
-            { description: "Memoria Justificativa", unit: "" },
-        ],
     };
 
     return (
