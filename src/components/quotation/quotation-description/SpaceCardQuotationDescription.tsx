7cd571387f889220e0caac008cc8985d9ce05846
Diego Rondón <furrtr4532@gmail.com>
2024-11-14 17:02:21 -0500

    style: ajustar estilos de texto y presentación en SpaceCardQuotationDescription

--- src/components/quotation/quotation-description/SpaceCardQuotationDescription.tsx
+++ src/components/quotation/quotation-description/SpaceCardQuotationDescription.tsx
@@ -20,21 +20,34 @@ export default function SpaceCardQuotationDescription({
             >
                 <CardContent className="p-4">
                     <div className="mb-2 flex items-start justify-between">
-                        <h3 className="text-lg font-semibold">{space.name}</h3>
-                        <Badge variant="outline" className="border-[#be2126]">
+                        <h3 className="text-sm font-light capitalize">
+                            {space.name}
+                        </h3>
+                        <Badge
+                            variant="outline"
+                            className="border-[#be2126] font-light"
+                        >
                             {level}
                         </Badge>
                     </div>
                     <div className="flex items-center justify-between">
                         <div className="flex items-center">
-                            <Maximize2 size={16} className="mr-2" />
-                            <span className="text-2xl font-bold">
+                            <Maximize2
+                                size={16}
+                                className="mr-2"
+                                strokeWidth={1.5}
+                            />
+                            <span className="text-base font-light">
                                 {space.area}
                             </span>
                             <span className="ml-1 text-sm">m²</span>
                         </div>
                         <div className="flex items-center">
-                            <Home size={16} className="mr-2" />
+                            <Home
+                                size={16}
+                                className="mr-2"
+                                strokeWidth={1.5}
+                            />
                             <span>{space.amount}</span>
                         </div>
                     </div>
