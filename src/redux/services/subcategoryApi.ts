4d087d5153c7c5f9239559abb2ef49510a24416c
Diego Rondón <furrtr4532@gmail.com>
2024-11-28 12:56:17 -0500

    feat: agregar API para gestionar subcategorías con operaciones CRUD

---
+++ src/redux/services/subcategoryApi.ts
@@ -0,0 +1,79 @@
+import { Subcategory } from "@/types";
+import { createApi } from "@reduxjs/toolkit/query/react";
+
+import baseQueryWithReauth from "../baseQuery";
+
+interface GetSubcategoryByIdProps {
+    id: string;
+}
+
+export const subcategoryApi = createApi({
+    reducerPath: "subcategoryApi",
+    baseQuery: baseQueryWithReauth,
+    tagTypes: ["Subcategory"],
+    endpoints: (build) => ({
+        //Crear subcategorias
+        createSubcategory: build.mutation<Subcategory, Partial<Subcategory>>({
+            query: (body) => ({
+                url: "/subcategory",
+                method: "POST",
+                body,
+                credentials: "include",
+            }),
+            invalidatesTags: ["Subcategory"],
+        }),
+        //Actualizar subcategorias
+        updateSubcategory: build.mutation<
+            Subcategory,
+            Partial<Subcategory> & { id: string }
+        >({
+            query: ({ id, ...body }) => ({
+                url: `/subcategory/${id}`,
+                method: "PATCH",
+                body,
+                credentials: "include",
+            }),
+            invalidatesTags: ["Subcategory"],
+        }),
+        //Obtener subcategoria por id
+        getSubcategoryById: build.query<Subcategory, GetSubcategoryByIdProps>({
+            query: ({ id }) => ({
+                url: `/subcategory/${id}`,
+                method: "GET",
+                credentials: "include",
+            }),
+            providesTags: ["Subcategory"],
+        }),
+        //Obtener todos los subcategorias
+        getAllSubcategory: build.query<Subcategory[], void>({
+            query: () => ({
+                url: "/subcategory",
+                method: "GET",
+                credentials: "include",
+            }),
+            providesTags: ["Subcategory"],
+        }),
+        //Eliminar subcategorias
+        deleteSubcategory: build.mutation<void, { ids: string[] }>({
+            query: (ids) => ({
+                url: `/subcategory/remove/all`,
+                method: "DELETE",
+                body: ids,
+                credentials: "include",
+            }),
+            invalidatesTags: ["Subcategory"],
+        }),
+        //Activar subcategorias
+        reactivateSubcategory: build.mutation<void, { ids: string[] }>({
+            query: (ids) => ({
+                url: `/subcategory/reactivate/all`,
+                method: "PATCH",
+                body: ids,
+                credentials: "include",
+            }),
+            invalidatesTags: ["Subcategory"],
+        }),
+    }),
+});
+
+export const {} = subcategoryApi;
