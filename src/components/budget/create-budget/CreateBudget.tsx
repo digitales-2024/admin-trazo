d19de5dc7c071078bb80f7d77b25aa5f07464eab
Diego Rondón <furrtr4532@gmail.com>
2024-11-29 16:54:45 -0500

    feat: agregar navegación y botones de acción en el formulario de creación de presupuestos

--- src/components/budget/create-budget/CreateBudget.tsx
+++ src/components/budget/create-budget/CreateBudget.tsx
@@ -2,13 +2,32 @@
 
 import { createBudgetSchema, CreateBudgetSchema } from "@/schemas";
 import { zodResolver } from "@hookform/resolvers/zod";
+/* import { RefreshCcw } from "lucide-react"; */
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
+
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 
 import { Form } from "../../ui/form";
 import BudgetCreator from "./create-detail-budget/BudgetCreator";
 import { HeadBudget } from "./create-head-budget/HeadBudget";
 
 export default function CreateBudget() {
+    const [isClient, setIsClient] = useState(false);
+    const router = useRouter();
+
+    useEffect(() => {
+        setIsClient(true);
+    }, []);
+
+    const handleBack = () => {
+        if (isClient) {
+            router.push("/execution-project/budgets");
+        }
+    };
+
     const form = useForm<CreateBudgetSchema>({
         resolver: zodResolver(createBudgetSchema),
         defaultValues: {
@@ -32,6 +51,30 @@ export default function CreateBudget() {
                 >
                     <HeadBudget form={form} />
                     <BudgetCreator />
+
+                    <Separator className="my-4" />
+
+                    <div className="flex flex-row-reverse gap-2 pt-2">
+                        <Button
+                            type="submit"
+                            /* disabled={isLoadingUpdateQuotation} */
+                        >
+                            {/*                             {isLoadingUpdateQuotation && (
+                                <RefreshCcw
+                                    className="mr-2 h-4 w-4 animate-spin"
+                                    aria-hidden="true"
+                                />
+                            )} */}
+                            Crear
+                        </Button>
+                        <Button
+                            type="button"
+                            variant={"destructive"}
+                            onClick={handleBack}
+                        >
+                            Cancelar
+                        </Button>
+                    </div>
                 </form>
             </Form>
         </>
