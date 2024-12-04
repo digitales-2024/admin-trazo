46506f6bc9db1e441e61da280b52d6aa4641bfb8
Diego Rondón <furrtr4532@gmail.com>
2024-12-04 16:14:35 -0500

    feat: agregar hook useApuBudget para gestionar la creación de presupuestos

---
+++ src/hooks/use-apu-budget.ts
@@ -0,0 +1,49 @@
+import { useCreateApuBudgetMutation } from "@/redux/services/apuBudgetApi";
+import { ApuBudget, CustomErrorData } from "@/types";
+import { translateError } from "@/utils/translateError";
+import { toast } from "sonner";
+
+export const useApuBudget = () => {
+    const [createApuBudget, { isSuccess: isSuccessCreateApuBudget }] =
+        useCreateApuBudgetMutation();
+
+    const onCreateApuBudget = async (input: Partial<ApuBudget>) => {
+        const promise = () =>
+            new Promise(async (resolve, reject) => {
+                try {
+                    const result = await createApuBudget(input);
+                    if (
+                        result.error &&
+                        typeof result.error === "object" &&
+                        "data" in result.error
+                    ) {
+                        const error = (result.error.data as CustomErrorData)
+                            .message;
+                        const message = translateError(error as string);
+                        reject(new Error(message));
+                    }
+                    if (result.error) {
+                        reject(
+                            new Error(
+                                "Ocurrió un error inesperado, por favor intenta de nuevo",
+                            ),
+                        );
+                    }
+                    resolve(result);
+                } catch (error) {
+                    reject(error);
+                }
+            });
+
+        return toast.promise(promise(), {
+            loading: "Creando análisis de precios unitarios...",
+            success: "Análisis de precios unitarios creado con éxito",
+            error: (err) => err.message,
+        });
+    };
+
+    return {
+        onCreateApuBudget,
+        isSuccessCreateApuBudget,
+    };
+};
