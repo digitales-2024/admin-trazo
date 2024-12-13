7f2cb7448e48871db3723db29a6dea72bdbd6300
Diego Rondon <82515256+DiegoRondonR@users.noreply.github.com>
2024-12-13 17:11:39 -0500

    Revert "Fix/revision errores para main"

--- src/components/common/text/TitleSecction.tsx
+++
@@ -1,7 +0,0 @@
-interface TitleSecctionProps {
-    text: string;
-}
-
-export const TitleSecction = ({ text }: TitleSecctionProps) => {
-    return <h1 className="text-4xl font-bold text-gray-900">{text}</h1>;
-};
