f9e2fe18ba6d060a486b7db0e7d52b98ddedba4c
Diego Rondón <furrtr4532@gmail.com>
2024-12-16 14:39:59 -0500

    fix: corregir el nombre de 'Equipos' a 'Herramientas' en el formulario de recursos APU

--- src/components/budget/create-budget/create-detail-budget/create-apu-budget/ApuResourceForm.tsx
+++ src/components/budget/create-budget/create-detail-budget/create-apu-budget/ApuResourceForm.tsx
@@ -64,7 +64,7 @@ export default function ApuResourceForm({
     };
 
     const resourceNameToType: { [key: string]: ResourceType } = {
-        Equipos: ResourceType.TOOLS,
+        Herramientas: ResourceType.TOOLS,
         "Mano de Obra": ResourceType.LABOR,
         Insumos: ResourceType.SUPPLIES,
         Servicios: ResourceType.SERVICES,
