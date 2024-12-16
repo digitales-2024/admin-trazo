35b680494ea70bebd033c5583b93c872830a1165
Diego Rondón <furrtr4532@gmail.com>
2024-12-16 11:08:47 -0500

    feat: agregar hook useExecutionProject para gestionar proyectos de ejecución

---
+++ src/hooks/use-execution-project.ts
@@ -0,0 +1,178 @@
+import {
+    useCreateExecutionProjectMutation,
+    useGetAllExecutionProjectsQuery,
+    useUpdateExecutionProjectMutation,
+    useUpdateStatusExecutionProjectMutation,
+} from "@/redux/services/executionProjectApi";
+import {
+    CustomErrorData,
+    ExecutionProject,
+    ExecutionProjectStatusType,
+} from "@/types";
+import { translateError } from "@/utils/translateError";
+import { toast } from "sonner";
+
+interface UseExecutionProjectProps {
+    id?: string;
+}
+
+export const useExecutionProject = (options: UseExecutionProjectProps = {}) => {
+    const { id } = options;
+    console.log("id", id);
+    const {
+        data: dataExecutionProjectsAll,
+        error,
+        isLoading,
+        isSuccess,
+        refetch,
+    } = useGetAllExecutionProjectsQuery();
+
+    const [
+        createExecutionProject,
+        { isSuccess: isSuccessCreateExecutionProject },
+    ] = useCreateExecutionProjectMutation();
+
+    const [
+        updateExecutionProjectStatus,
+        { isSuccess: isSuccessUpdateExecutionProjectStatus },
+    ] = useUpdateStatusExecutionProjectMutation();
+
+    const [
+        updateExecutionProject,
+        {
+            isSuccess: isSuccessUpdateExecutionProject,
+            isLoading: isLoadingUpdateExecutionProject,
+        },
+    ] = useUpdateExecutionProjectMutation();
+
+    const onCreateExecutionProject = async (
+        input: Partial<ExecutionProject>,
+    ) => {
+        const promise = () =>
+            new Promise(async (resolve, reject) => {
+                try {
+                    const result = await createExecutionProject(input);
+                    if (result.error) {
+                        if (
+                            typeof result.error === "object" &&
+                            "data" in result.error
+                        ) {
+                            const error = (result.error.data as CustomErrorData)
+                                .message;
+                            const message = translateError(error as string);
+                            reject(new Error(message));
+                        } else {
+                            reject(
+                                new Error(
+                                    "Ocurrió un error inesperado, por favor intenta de nuevo",
+                                ),
+                            );
+                        }
+                    } else {
+                        resolve(result);
+                    }
+                } catch (error) {
+                    reject(error);
+                }
+            });
+
+        return toast.promise(promise(), {
+            loading: "Creando proyecto de ejecución...",
+            success: "Proyecto de ejecución creado con éxito",
+            error: (err) => err.message,
+        });
+    };
+
+    const onUpdateExecutionProjectStatus = async (data: {
+        body: ExecutionProjectStatusType;
+        id: string;
+    }) => {
+        const promise = () =>
+            new Promise(async (resolve, reject) => {
+                try {
+                    const result = await updateExecutionProjectStatus(data);
+                    if (result.error) {
+                        if (
+                            typeof result.error === "object" &&
+                            "data" in result.error
+                        ) {
+                            const error = (result.error.data as CustomErrorData)
+                                .message;
+                            const message = translateError(error as string);
+                            reject(new Error(message));
+                        } else {
+                            reject(
+                                new Error(
+                                    "Ocurrió un error inesperado, por favor intenta de nuevo",
+                                ),
+                            );
+                        }
+                    } else {
+                        resolve(result);
+                    }
+                } catch (error) {
+                    reject(error);
+                }
+            });
+
+        return toast.promise(promise(), {
+            loading: "Actualizando estado de proyecto...",
+            success: "Estado de proyecto actualizado con éxito",
+            error: (err) => err.message,
+        });
+    };
+
+    const onUpdateExecutionProject = async (
+        input: Partial<ExecutionProject> & { id: string },
+    ) => {
+        const promise = () =>
+            new Promise(async (resolve, reject) => {
+                try {
+                    const result = await updateExecutionProject(input);
+                    if (
+                        result.error &&
+                        typeof result.error === "object" &&
+                        result.error !== null &&
+                        "data" in result.error
+                    ) {
+                        const error = (result.error.data as CustomErrorData)
+                            .message;
+                        const message = translateError(error as string);
+                        reject(new Error(message));
+                    }
+                    if (result.error) {
+                        reject(
+                            new Error(
+                                "Ocurrió un error inesperado, por favor intenta de nuevo",
+                            ),
+                        );
+                    }
+                    resolve(result);
+                } catch (error) {
+                    reject(error);
+                }
+            });
+        toast.promise(promise(), {
+            loading: "Actualizando proyecto de ejecución...",
+            success: "Proyecto de ejecución actualizado exitosamente",
+            error: (error) => {
+                return error.message;
+            },
+        });
+    };
+
+    return {
+        dataExecutionProjectsAll,
+        error,
+        isLoading,
+        isSuccess,
+        refetch,
+        onCreateExecutionProject,
+        onUpdateExecutionProjectStatus,
+        onUpdateExecutionProject,
+        isSuccessCreateExecutionProject,
+        isSuccessUpdateExecutionProjectStatus,
+        isSuccessUpdateExecutionProject,
+        isLoadingUpdateExecutionProject,
+    };
+};
